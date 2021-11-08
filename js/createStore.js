0971ce6ce557994d1eebb0f3bac967301da76f95
sergiocardenasg <sergiocardenas@utexas.edu>
2021-11-07 18:24:43 -0600

    Revert "solved"
    This reverts commit 19c5de1722e213a1df6691de90950ba91128cf9a.

--- js/createStore.js
+++ js/createStore.js
@@ -1,20 +1,4 @@
-function createStore(reducer) {
-  let state;
-
-  function dispatch(action) {
-    state = reducer(state, action);
-    render();
-  }
-
-  function getState() {
-    return state;
-  };
-
-  return {
-    dispatch,
-    getState
-  };
-};
+let state;
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -24,17 +8,21 @@ function reducer(state = { count: 0 }, action) {
     default:
       return state;
   }
-}
+};
+
+function dispatch(action){
+  state = reducer(state, action);
+  render();
+};
 
 function render() {
   let container = document.getElementById('container');
-  container.textContent = store.getState().count;
+  container.textContent = state.count;
 };
 
-let store = createStore(reducer) // createStore takes the reducer as an argument
-store.dispatch({ type: '@@INIT' });
+dispatch({ type: '@@INIT' })
 let button = document.getElementById('button');
 
-button.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREASE_COUNT' });
-});
+button.addEventListener('click', function() {
+    dispatch({ type: 'INCREASE_COUNT' });
+})
